f50fbe669be51feb7d52c9606cf2604a9d52c06e
Sebastian <Sebastian@Strobl.net>
2021-03-03 19:04:24 +0100

    Add: Operators

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#[cfg(test)]
+mod operators;
 mod tests {
     #[test]
     fn it_works() {
---
+++ src/operators.rs
@@ -0,0 +1,11 @@
+pub trait Operator: Copy { }
+
+#[derive(Copy, Clone)]
+pub struct Additive { }
+
+#[derive(Copy, Clone)]
+pub struct Multiplicative { }
+
+impl Operator for Additive { }
+
+impl Operator for Multiplicative { }
