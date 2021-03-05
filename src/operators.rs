abff86d57c84dd1e7885a97e4b69467707fd1581
Sebastian <Sebastian@Strobl.net>
2021-03-05 04:13:00 +0100

    Add: predefined closed operations

--- src/operators.rs
+++ src/operators.rs
@@ -1,3 +1,6 @@
+use std::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Neg, Sub, SubAssign};
+use std::process::Output;
+
 pub trait Operator: Copy {}
 
 #[derive(Copy, Clone)]
@@ -9,3 +12,9 @@ pub struct Multiplicative {}
 impl Operator for Additive {}
 
 impl Operator for Multiplicative {}
+
+pub trait ClosedAdd<Rhs = Self>: Add<Rhs, Output = Self> + AddAssign<Rhs> {}
+pub trait ClosedSub<Rhs = Self>: Sub<Rhs, Output = Self> + SubAssign<Rhs> {}
+pub trait ClosedMul<Rhs = Self>: Mul<Rhs, Output = Self> + MulAssign<Rhs> {}
+pub trait ClosedDiv<Rhs = Self>: Div<Rhs, Output = Self> + DivAssign<Rhs> {}
+pub trait ClosedNeg: Neg<Output = Self> {}
