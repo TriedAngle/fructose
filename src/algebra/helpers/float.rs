289b05c2dd1e53871cd18d1a9f65244b1a5b3aba
Sebastian <Sebastian@Strobl.net>
2021-03-12 15:47:18 +0100

    Change: version num & Add: impl Float trait

--- src/algebra/helpers/float.rs
+++ src/algebra/helpers/float.rs
@@ -1,14 +1,43 @@
-pub trait FloatBounded {
+use std::num::FpCategory;
+
+pub trait Float {
     const EPSILON: Self;
-    const MAX_POSITIVE: Self;
+    const MIN: Self;
     const MIN_POSITIVE: Self;
+    const MAX: Self;
     const INFINITY: Self;
     const NEG_INFINITY: Self;
     const NAN: Self;
 
-    fn is_infinite(&self) -> bool;
-    fn is_nan(&self) -> bool;
-    fn is_sub_normal(&self) -> bool;
-    fn is_normal(&self) -> bool;
-    fn classify(&self) -> bool;
+    fn is_infinite(self) -> bool;
+    fn is_nan(self) -> bool;
+    fn is_normal(self) -> bool;
+    fn classify(self) -> FpCategory;
 }
+
+macro_rules! impl_float {
+    ($($set:ty)*) => {
+        $(
+            impl Float for $set {
+                forward! {
+                    const EPSILON: Self;
+                    const MIN: Self;
+                    const MIN_POSITIVE: Self;
+                    const MAX: Self;
+                    const INFINITY: Self;
+                    const NEG_INFINITY: Self;
+                    const NAN: Self;
+                }
+
+                forward! {
+                    fn is_infinite(self) -> bool;
+                    fn is_nan(self) -> bool;
+                    fn is_normal(self) -> bool;
+                    fn classify(self) -> FpCategory;
+                }
+            }
+        )*
+    }
+}
+
+impl_float!(f32 f64);
