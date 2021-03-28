362500ff6dfb9f185405dd5fc5ff3e4784e0f60d
Sebastian <Sebastian@Strobl.net>
2021-03-28 18:22:11 +0200

    Fix: ListSet & Add: WholeListSet

--- src/algebra/helpers/list.rs
+++ src/algebra/helpers/list.rs
@@ -1,7 +1,13 @@
+use crate::algebra::helpers::bound::Bounded;
 use std::ops::Range;
 
 pub trait ListSet<Rhs = Self>: Sized {
-    fn list_set(range: Range<Rhs>) -> Vec<Self>;
+    fn list_set(range: Range<Rhs>) -> Vec<Rhs>;
+}
+
+// this trait is not recommended to be used for big sets
+pub trait WholeListSet<Rhs = Self>: Bounded<Rhs> {
+    fn whole_list_set() -> Vec<Rhs>;
 }
 
 macro_rules! impl_list_set {
@@ -9,7 +15,7 @@ macro_rules! impl_list_set {
         $(
             impl ListSet for $set {
                 #[inline]
-                fn list_set(range: Range<$set>) -> Vec<Self> {
+                fn list_set(range: Range<$set>) -> Vec<$set> {
                     let mut set = Vec::new();
                     for val in range {
                         set.push(val);
